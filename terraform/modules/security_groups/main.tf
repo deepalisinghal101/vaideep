d51876cda6b0da2e89c91df669cffc017a1e155f
deepalisinghal101 <deepalisinghal16@gmail.com>
2026-07-10 04:37:47 +0530

    Remove Jenkins and Kafka UI security groups
    Removed Jenkins and Kafka UI security groups and their associated rules. Updated Kafka security group ingress description.

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -21,56 +21,17 @@ resource "aws_security_group" "bastion" {
   tags = { Name = "${var.project_name}-bastion-sg", Environment = var.environment }
 }
 
-resource "aws_security_group" "jenkins" {
-  name        = "${var.project_name}-jenkins-sg"
-  description = "Jenkins server"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "Jenkins UI"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description     = "SSH from Bastion"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = { Name = "${var.project_name}-jenkins-sg", Environment = var.environment }
-}
-
 resource "aws_security_group" "kafka" {
   name        = "${var.project_name}-kafka-sg"
   description = "Kafka brokers"
   vpc_id      = var.vpc_id
 
   ingress {
-    description     = "SSH from Bastion"
+    description     = "SSH from Bastion (also used by Ansible via ProxyJump)"
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion.id]
-  }
-
-  ingress {
-    description     = "SSH from Jenkins (Ansible)"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.jenkins.id]
   }
 
   ingress {
@@ -89,14 +50,6 @@ resource "aws_security_group" "kafka" {
     self        = true
   }
 
-  ingress {
-    description     = "Kafka from Kafka UI"
-    from_port       = 9092
-    to_port         = 9092
-    protocol        = "tcp"
-    security_groups = [aws_security_group.kafka_ui.id]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -105,35 +58,4 @@ resource "aws_security_group" "kafka" {
   }
 
   tags = { Name = "${var.project_name}-kafka-sg", Environment = var.environment }
-}
-
-resource "aws_security_group" "kafka_ui" {
-  name        = "${var.project_name}-kafka-ui-sg"
-  description = "Kafka UI dashboard"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "Kafka UI web port"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description     = "SSH from Bastion"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = { Name = "${var.project_name}-kafka-ui-sg", Environment = var.environment }
 }
